bcbccdfe5442d62f4ddaecbae4f03dd1eee1cdb8
zmeriksen <zmeriksen@icloud.com>
2020-06-08 17:08:12 -0500

    Initial Commit

---
+++ SUIBT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SUIBT
+//
+//  Created by Zach Eriksen on 6/8/20.
+//  Copyright © 2020 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
